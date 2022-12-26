b57e6ee4d6d288105f067acc803d156d8c79dd17
kaushal damani <kaushaldamani386@gmail.com>
2022-12-26 11:06:08 +0530

    node

--- Wheather/Wheather.js
+++ Wheather/Wheather.js
@@ -5,27 +5,22 @@ const weather = () => {
   const API = '913a2479ae4cc43d7d8c11bd8c802a87';
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`;
 
-  axios
-    .get(url)
-    .then((result) => {
-      const data = result.data.main;
-      // const temp = data.temp;
-      // const pressure = data.pressure;
-      // console.log(data);
+  axios.get(url).then((result) => {
+    const data = result.data.main;
+    // const temp = data.temp;
+    // const pressure = data.pressure;
+    // console.log(data);
 
-      const cityname = result.data.name;
-      console.log(cityname);
-    })
+    const cityname = result.data.name;
+    console.log(cityname);
+  });
 
-    callback({
-        temp : data.temp,
-        city: cityname,
-        pressure : data.pressure
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  callback({
+    temp: data.temp,
+    city: cityname,
+    pressure: data.pressure,
+  }).catch((err) => {
+    console.log(err);
+  });
 };
 weather();
-
-
